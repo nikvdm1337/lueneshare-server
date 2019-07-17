34e04f1c81c13f82d47151522563211d3d51f0d1
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-17 14:42:34 +0700

    connecting to localhost

---
+++ index.js
@@ -0,0 +1,21 @@
+// Config, importing the modules
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser')
+require('dotenv').config()
+const cors = require('cors')
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+app.use(cors())
+
+app.listen(process.env.PORT, (err) => {
+    if (err) {
+    console.log('ERROR', err)
+    } else {
+    console.log(`Connected to localghost ${process.env.PORT}`)
+    }
+})
