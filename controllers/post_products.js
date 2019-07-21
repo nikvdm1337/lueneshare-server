e438e7ea64f0d526ce77cafaf3f5df7dc49ad7b8
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-21 12:41:38 +0700

    controller for post_product

---
+++ controllers/post_products.js
@@ -0,0 +1,31 @@
+const db_product = require('../models/product')
+const jwt = require('jsonwebtoken')
+
+module.exports = (req, res) => {
+	let token = req.headers.authorization.split(' ')[1]
+	
+	jwt.verify(token, process.env.SECRET, (err, decoded) => {
+		if (decoded) {
+			console.log('decoded', decoded)
+			console.log('req.body', req.body)
+			req.body.author = decoded._id
+			db_product.create(req.body).then((data) => {
+				db_product.findById(data._id)
+					.populate({
+						path: 'name',
+						select: 'name'
+					}).populate({
+						path: 'category',
+						select: 'name'
+					}).then((message) => {
+						res.send(message)
+					}).catch((err) => {
+						res.send(err)
+					})
+			}).catch((err) => {
+				console.log('err', err)
+				res.send(err)
+			})
+		}
+	})
+}
