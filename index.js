8260213329a374b3948197db87ec701ec9dd7cb5
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-31 02:32:40 +0700

    added route, deleting multiple dotenv

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const express = require('express')
 const app = express()
 var bodyParser = require('body-parser')
-require('dotenv').config()
 const cors = require('cors')
+const multer = require('multer')
+require('dotenv').config()
 
 // parse application/x-www-form-urlencoded
+app.use(cors({credentials:true}))
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
@@ -18,7 +20,7 @@ app.get('/api/messages', require('./controllers/get_messages'))
 app.get('/api/profile', require('./controllers/get_profile'))
 
 app.post('/api/signup', require('./controllers/post_user'))
-app.post('/api/products', require('./controllers/post_products'))
+app.post('/api/products', multer({ storage: multer.memoryStorage() }).single('file'), require('./controllers/post_products'))
 app.post('/api/login', require('./controllers/login'))
 app.post('/api/messages', require ('./controllers/post_message'))
 
