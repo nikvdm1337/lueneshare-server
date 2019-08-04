b2a182b96ec72c5ded566a7e6913089c89aa4ebb
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-08-04 17:46:03 +0700

    working on aggregation of products per user

--- controllers/get_messages.js
+++ controllers/get_messages.js
@@ -1,14 +1,24 @@
 const db_message = require('../models/message')
+const _ = require('lodash')
 
 module.exports = (req, res) => {
-	db_message.find(req.query).sort('-date').populate({
+	let q = {}
+	console.log(req.query)
+	if (req.query && req.query.author) {
+		q.author = req.query.author
+	} else {
+		let q = {}
+	}
+	db_message.find(q).sort('-date').populate({
 		path: 'category',
 		select: 'name'
 	}).populate({
 		path: 'author',
 		select: 'name'
 	}).then((data) => {
-		res.send(data)
+		data_grouped = _.uniqBy(data, 'author._id')
+		console.log('data', data_grouped);
+		res.send(data_grouped)
 	}).catch((err) => {
 		res.send(err)
 	})
