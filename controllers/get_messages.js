84d14d4806aa84459922b9bdf74ad6e69b17c4a4
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-08-05 00:30:52 +0700

    not aggregating anymore

--- controllers/get_messages.js
+++ controllers/get_messages.js
@@ -2,23 +2,23 @@ const db_message = require('../models/message')
 const _ = require('lodash')
 
 module.exports = (req, res) => {
-	let q = {}
-	console.log(req.query)
-	if (req.query && req.query.author) {
-		q.author = req.query.author
-	} else {
-		let q = {}
-	}
-	db_message.find(q).sort('-date').populate({
+	// let q = {}
+	// console.log(req.query)
+	// if (req.query && req.query.author) {
+	// 	q.author = req.query.author
+	// } else {
+	// 	let q = {}
+	// }
+	db_message.find(req.query).sort('-date').populate({
 		path: 'category',
 		select: 'name'
 	}).populate({
 		path: 'author',
 		select: 'name'
 	}).then((data) => {
-		data_grouped = _.uniqBy(data, 'author._id')
-		console.log('data', data_grouped);
-		res.send(data_grouped)
+		// data_grouped = _.uniqBy(data, 'author._id')
+		// console.log('data', data_grouped);
+		res.send(data)
 	}).catch((err) => {
 		res.send(err)
 	})
